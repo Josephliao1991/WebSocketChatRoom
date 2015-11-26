2ca21f896ac67a01bbd6ee47f00da1e830c98429
Josephliao1991 <gn02239440@gmail.com>
2015-11-26 16:23:17 +0800

    1. Add Key Function

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ var http = require("http")
 var ws = require("nodejs-websocket")
 var fs = require("fs")
 
+
+//KEY
+var alice = "alice123"
+var kefan = "kefan456"
+var pi		= "pi789"
+var doris = "doris123"
+var allen	= "allen456"
+var joseph = "joseph789"
+var ivan  = "ivan123"
+var dannies = "dannies456"
+var jimmy = "jimmy789"
+
+//NAME
+var ALICE = "硝戎魚"
+var KEFAN = "剪嗑煩"
+var PI		= "湃萃嗑"
+var DORIS = "躲粒撕"
+var ALLEN	= "誒任"
+var JOSEPH = "阿廖"
+var IVAN  = "挨悶"
+var DANNIES = "亡尾菌"
+var JIMMY = "你加薪"
+
 var historyMaxCount = 30
 var historyMessage = []
 
@@ -13,13 +36,19 @@ var server = ws.createServer(function (connection) {
 	connection.nickname = null
 	connection.on("text", function (str) {
 		if (connection.nickname === null) {
-			connection.nickname = str
-			broadcast("["+str+"] 進入聊天室")
-			console.log("["+str+"] 進入聊天室");
+			//Check User Login
+			var key = checkUserLogin(str)
+			if (key == null) {
+				return connection.sendText("loginFail")
+			}
+
+			connection.nickname = key
+			broadcast("["+key+"] 進入聊天室")
+			console.log("["+key+"] 進入聊天室");
 
 			if(historyMessage.length > 0) {
-				historyMessage.forEach(function (str) {
-					connection.sendText(str)
+				historyMessage.forEach(function (key) {
+					connection.sendText(key)
 				})
 			}
 
@@ -55,4 +84,31 @@ function broadcast(str) {
 		server.connections.forEach(function (connection) {
 			connection.sendText(str)
 		})
+}
+
+
+function checkUserLogin(key) {
+	// body...
+
+	if (key == alice){
+		return ALICE
+	}else if(key == kefan){
+		return KEFAN
+	}else if (key == pi) {
+		return PI
+	}else if (key == doris) {
+		return DORIS
+	}else if (key == allen) {
+		return ALLEN
+	}else if (key == joseph) {
+		return JOSEPH
+	}else if (key == ivan) {
+		return IVAN
+	}else if (key == dannies) {
+		return DANNIES
+	}else if (key == jimmy) {
+		return JIMMY
+	}else {
+		return null
+	}
 }
